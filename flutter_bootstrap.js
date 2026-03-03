5846b63f9da7b0f543af6357654a852da47aabc5
duckhiem2003 <duckhiem2003@gmail.com>
2026-03-03 11:38:31 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3344126664"
+    serviceWorkerVersion: "1507724902"
   }
 });
